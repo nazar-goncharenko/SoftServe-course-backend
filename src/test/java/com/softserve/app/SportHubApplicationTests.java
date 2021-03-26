7c5fc2ed898405ef2c2f9915de8c6631b9738615
ryba147 <54861049+ryba147@users.noreply.github.com>
2021-03-26 20:02:48 +0200

    Project setup

---
+++ src/test/java/com/softserve/app/SportHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserve.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
