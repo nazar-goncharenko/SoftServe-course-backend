ad95073b072bb256edf5e842cfc102ae0b9a0bbd
Vira Maksymets <55591954+ViraMaximets@users.noreply.github.com>
2021-03-29 23:15:23 +0300

    db models
    Co-Authored-By: techsky <55064294+nazar-goncharenko@users.noreply.github.com>

---
+++ src/main/java/com/softserve/app/Models/Option.java
@@ -0,0 +1,54 @@
+package com.softserve.app.Models;
+
+import javax.persistence.*;
+
+@Entity
+public class Option {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    private Survey survey;
+
+    @Column(name = "text", nullable = false)
+    private String text;
+
+    @Column(name = "responses_count", nullable = false)
+    private Long responses_count;
+
+    public Option() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Survey getSurvey() {
+        return survey;
+    }
+
+    public void setSurvey(Survey survey) {
+        this.survey = survey;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Long getResponses_count() {
+        return responses_count;
+    }
+
+    public void setResponses_count(Long responses_count) {
+        this.responses_count = responses_count;
+    }
+}
---
+++ src/main/java/com/softserve/app/Models/User.java
@@ -0,0 +1,114 @@
+package com.softserve.app.Models;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    private enum Role {
+        ADMIN, USER;
+    }
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<Survey> userSurveys = new HashSet<>();
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<Banner> userBanners = new HashSet<>();
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<Comment> userComments = new HashSet<>();
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<SportCategory> favourites = new HashSet<>();
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Set<Survey> getUserSurveys() {
+        return userSurveys;
+    }
+
+    public void setUserSurveys(Set<Survey> userSurveys) {
+        this.userSurveys = userSurveys;
+    }
+
+    public Set<Banner> getUserBanners() {
+        return userBanners;
+    }
+
+    public void setUserBanners(Set<Banner> userBanners) {
+        this.userBanners = userBanners;
+    }
+
+    public Set<Comment> getUserComments() {
+        return userComments;
+    }
+
+    public void setUserComments(Set<Comment> userComments) {
+        this.userComments = userComments;
+    }
+
+    public Set<SportCategory> getFavourites() {
+        return favourites;
+    }
+
+    public void setFavourites(Set<SportCategory> favourites) {
+        this.favourites = favourites;
+    }
+}
