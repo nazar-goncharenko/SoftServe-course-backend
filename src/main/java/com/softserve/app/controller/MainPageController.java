096a6c75798126a015afeff99935e3eea2b1c521
Vira Maksymets <55591954+ViraMaximets@users.noreply.github.com>
2021-04-09 22:09:29 +0300

    add anotations

--- src/main/java/com/softserve/app/controller/MainPageController.java
+++ src/main/java/com/softserve/app/controller/MainPageController.java
@@ -1,8 +1,8 @@
 package com.softserve.app.controller;
 
 import com.softserve.app.models.User;
-import com.softserve.app.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.softserve.app.service.UserService.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,20 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 public class MainPageController {
 
     private final UserService userService;
 
-    @Autowired
-    public MainPageController(UserService userService) {
-        this.userService = userService;
-    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<String> showMainPage() {
         return  ResponseEntity.ok("You are at main page ^^");
     }
+
 
     // just for testing
     @RequestMapping(value = "/getUsers", method = RequestMethod.GET)
