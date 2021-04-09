a5753423aa2c40a73795352c1e4a6fdb6fd707ac
Vira Maksymets <55591954+ViraMaximets@users.noreply.github.com>
2021-04-09 22:00:04 +0300

    Update MainPageController.java

--- src/main/java/com/softserve/app/controller/MainPageController.java
+++ src/main/java/com/softserve/app/controller/MainPageController.java
@@ -1,18 +1,18 @@
 package com.softserve.app.controller;
 
 import com.softserve.app.models.User;
-import com.softserve.app.service.UserService.UserService;
+import com.softserve.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class MainPageController {
 
     private final UserService userService;
