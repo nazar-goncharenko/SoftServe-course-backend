59269cc96209cd0574abccabd5b9a88b23a631d1
Vira Maksymets <55591954+ViraMaximets@users.noreply.github.com>
2021-04-09 22:24:48 +0300

    optimized imports

--- src/main/java/com/softserve/app/controller/UserController.java
+++ src/main/java/com/softserve/app/controller/UserController.java
@@ -7,7 +7,15 @@ import com.softserve.app.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Objects;
