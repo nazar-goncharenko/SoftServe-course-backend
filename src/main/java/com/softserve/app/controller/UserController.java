94b7ad3a35d567718afb0c8556ca17655a82710a
Vira Maksymets <55591954+ViraMaximets@users.noreply.github.com>
2021-04-11 21:28:26 +0300

    returned login methods

--- src/main/java/com/softserve/app/controller/UserController.java
+++ src/main/java/com/softserve/app/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.softserve.app.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.softserve.app.constant.SportHubConstant;
 import com.softserve.app.dto.UserDTO;
 import com.softserve.app.exception.SportHubException;
+import com.softserve.app.models.ResetPasswordRequest;
 import com.softserve.app.models.User;
+import com.softserve.app.models.View;
+import com.softserve.app.service.ResetService;
 import com.softserve.app.service.UserService.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Objects;
 
 
@@ -66,5 +73,53 @@ public class UserController {
             return userService.updateAvatar(user_id, userAva);
         }
         throw new SportHubException(SportHubConstant.AUTHORIZE_EXCEPTION.getMessage(), 403);
+    }
+
+
+    // Registration
+
+
+    private final ResetService resetService;
+    @PostMapping("/registration")
+    @JsonView(View.UserInfo.class)
+    public ResponseEntity<UserDTO> addUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return new ResponseEntity<>(user.ofDTO(), HttpStatus.OK);
+    }
+
+    // just for test
+    @GetMapping("/users")
+    List<User> getAllUser() {
+        return userService.findAll();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserDTO> loginAuth(@RequestBody UserDTO userDTO) {
+        userService.authorize(userDTO);
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+
+    @ApiOperation(value = "Get email for reset",
+            notes = "Send message to your email\"")
+    @PostMapping("/forgot_password")
+    public ResponseEntity<UserDTO> processForgotPassword(
+            @ApiParam(
+                    name = "email",
+                    type = "String",
+                    value = "User Email",
+                    example = "example@gmail.com",
+                    required = true) @RequestBody UserDTO userDTO) {
+        resetService.createToken(userDTO);
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Reset password",
+            notes = "Input passwords\"")
+    @PostMapping("/reset_password")
+    public ResponseEntity<UserDTO> processResetPassword(
+            @RequestBody ResetPasswordRequest resetPasswordRequest) {
+        resetService.resetPassword(resetPasswordRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
