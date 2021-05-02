318a89306002943c297f67d3a8d68aef3e437c12
techsky <55064294+nazar-goncharenko@users.noreply.github.com>
2021-05-02 21:13:55 +0300

    fix bugs

--- src/main/java/com/softserve/app/dto/UserDTO.java
+++ src/main/java/com/softserve/app/dto/UserDTO.java
@@ -2,6 +2,7 @@ package com.softserve.app.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.softserve.app.models.Comment;
+import com.softserve.app.models.SportCategory;
 import com.softserve.app.models.Survey;
 import com.softserve.app.models.User;
 import lombok.AllArgsConstructor;
@@ -12,8 +13,12 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.logging.Handler;
+import java.util.stream.Collectors;
 
 @Data
 @EqualsAndHashCode
@@ -50,9 +55,13 @@ public class UserDTO implements Serializable {
                 .email(this.email)
                 .password(this.new_pass_2)
                 .photoUrl(this.photoUrl)
-                .favourites((Set<SportCategory>) this.favourites)
-                .userComments((Set<Comment>) this.userComments)
-                .userSurveys((Set<Survey>) this.userSurveys)
+                .favourites(new HashSet<>(
+                        this.favourites.stream()
+                                .map(SportCategoryDTO::ofEntity)
+                                .collect(Collectors.toList())
+                ))
+                .userComments(new HashSet<>(this.userComments))
+                .userSurveys(new HashSet<>(this.userSurveys))
                 .build();
     }
 }
