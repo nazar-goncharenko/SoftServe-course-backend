45b19e1299ed2b259c7af4276e1d1640c631291f
no-code-no-life <vladp_2001@ukr.net>
2021-05-05 18:34:08 +0300

    after patched resolved

--- src/main/java/com/softserve/app/security/CustomFilter.java
+++ src/main/java/com/softserve/app/security/CustomFilter.java
@@ -14,7 +14,6 @@ public class CustomFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         chain.doFilter(request, response);
-        HttpServletResponse  resp= (HttpServletResponse) response;
     }
 
 }
