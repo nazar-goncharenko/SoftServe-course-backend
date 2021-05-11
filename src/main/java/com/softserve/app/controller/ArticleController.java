1cfe75041cb2b671c0ac83ec28bf603754fa2438
ryba147 <54861049+ryba147@users.noreply.github.com>
2021-05-11 07:36:06 +0300

    Multipart file import

--- src/main/java/com/softserve/app/controller/ArticleController.java
+++ src/main/java/com/softserve/app/controller/ArticleController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
 @RestController
